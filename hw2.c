784c38bd4f6cee445da2167803cbb4666aa46c12
Jimmy Lazo <astrojlazo@gmail.com>
2017-02-01 23:19:52 -0600

    Comments and a way to check if the file open
    Added more comments and section that checks if the file is available or has write permissions.

--- hw2.c
+++ hw2.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+/*This function takes two command line arguments.*/
+/*ex: ./hw2 life 42 */
+/* First argument "life" names the file that is written.*/
+/* Second argument "42" is the number of lines it will eventually fill with randomly generated numbers.*/
+
 
 void generate_data(char *filename,int number_to_write)
 /*This function is all good */
@@ -19,7 +24,13 @@ double calculate_average(char *filename)
    int number_of_lines=0;
    FILE *fp;
    fp=fopen(filename,"r");
-   while (!feof(fp))
+   if(fp==NULL) /* To check if the file is actually there */
+    {
+	perror("File not found?");
+	return(-1);
+    }
+
+   while (!feof(fp)) 
     {
       fscanf(fp,"%lf",&x);
       average+=x;
@@ -39,12 +50,11 @@ int main(int argc, char *argv[])
         printf("Not enough arguments\n");
         exit(0); /*We're out of here! */
      }
-   strcpy(fname,argv[1]);
-   N=atoi(argv[2]);
-   generate_data(fname,N);
-   average=calculate_average(fname);
+   strcpy(fname,argv[1]); /* Assigns first argument and assigns it to fname*/
+   N=atoi(argv[2]); /* Converts the second argument into an integer, assigns it to N */
+   generate_data(fname,N); /* Both arguments are passed to a function that generates an "N" amount of lines of randomly
+	generated numbers into a file named by "fname"*/
+   average=calculate_average(fname); /* Finds the sum ("average") of all the numbers and divides it by the 		"number_of_lines"*/
    printf("The average of these machinations is %f\n",average);
    return(0);
 }
-
-
